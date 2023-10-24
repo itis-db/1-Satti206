fee85b65a01d684f8d3203f95c775b01ac48073f
Satti Sakaeva <112933814+Satti206@users.noreply.github.com>
2023-10-24 22:52:19 +0300

    3 level

--- data.sql
+++ data.sql
@@ -1,17 +1,32 @@
-    insert into activitytype(
-    	  activitytypeid
-        ,name
-        ,sysname
-    )
-    select d.*
-    from (
-    	values(1,'Программа','Program')
-        ,(2,'Подпрограмма','SubProgram')
-        ,(3,'Проект','Project')
-        ,(4,'Контракт','Contract')
-        ,(5,'КТ','Point')
-    ) d(activitytypeid, name, sysname)
-    where not exists(
-    	select 1 from activitytype t
-        where t.activitytypeid = d.activitytypeid
-    );
+INSERT INTO ActivityType (ActivityTypeId, TypeName)
+VALUES
+    (1, 'Программа'),
+    (2, 'Подпрограмма'),
+    (3, 'Проект'),
+    (4, 'Контракт'),
+    (5, 'Контрольная точка');
+
+INSERT INTO Area (AreaId, AreaName)
+VALUES
+    (1, 'Территория 1'),
+    (2, 'Территория 2');
+
+INSERT INTO Activity (ActivityId, ParentId, ActivityName, ActivityTypeId, AreaId)
+VALUES
+    (1, NULL, 'Программа 1', 1, 1),
+    (2, 1, 'Подпрограмма 1.1', 2, 1),
+    (3, 1, 'Подпрограмма 1.2', 2, 1),
+    (4, 2, 'Проект 1.1.1', 3, 1),
+    (5, 2, 'Проект 1.1.2', 3, 1),
+    (6, 3, 'Проект 1.2.1', 3, 1),
+    (20, NULL, 'Программа 2', 1, 2),
+
+INSERT INTO Contract (ContractId, ActivityId, ContractName, AreaId)
+VALUES
+    (1, 4, 'Контракт 1.1.1.1', 1),
+    (2, 5, 'Контракт 1.1.2.1', 1),
+
+INSERT INTO Point (PointId, ActivityId, PointName)
+VALUES
+    (1, 4, 'КТ 1.1.1.1.1'),
+    (2, 4, 'КТ 1.1.1.1.2'),
