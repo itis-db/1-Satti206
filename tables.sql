5566d0773642087e7686bbdfb339a762e0724b9c
Satti Sakaeva <112933814+Satti206@users.noreply.github.com>
2023-10-24 22:49:30 +0300

    3 level

--- tables.sql
+++ tables.sql
@@ -1,11 +1,44 @@
-
-create table if not exists activitytype
-(
-    activitytypeid integer not null
-        constraint pk_activitytype
-            primary key,
-    name           text    not null,
-    sysname        text    not null
+CREATE TABLE ActivityType (
+    ActivityTypeId INT PRIMARY KEY,
+    TypeName NVARCHAR(255)
 );
 
--- 
+CREATE TABLE Activity (
+    ActivityId INT PRIMARY KEY,
+    ParentId INT,
+    ActivityName NVARCHAR(255),
+    ActivityTypeId INT,
+    AreaId INT
+);
+
+CREATE TABLE Program (
+    ProgramId INT PRIMARY KEY,
+    ActivityId INT PRIMARY KEY,
+    ProgramName NVARCHAR(255),
+);
+
+CREATE TABLE SubProgram (
+    SubProgramId INT PRIMARY KEY,
+    ActivityId INT PRIMARY KEY,
+    SubProgramName NVARCHAR(255),
+);
+
+CREATE TABLE Project (
+    ProjectId INT PRIMARY KEY,
+    ActivityId INT PRIMARY KEY,
+    ProjectName NVARCHAR(255),
+);
+
+CREATE TABLE Contract (
+    ContractId INT PRIMARY KEY,
+    ActivityId INT PRIMARY KEY,
+    ContractName NVARCHAR(255),
+    AreaId INT,
+);
+
+CREATE TABLE Point (
+    PointId INT PRIMARY KEY,
+    ActivityId INT PRIMARY KEY,
+    PointName NVARCHAR(255),
+);
+
